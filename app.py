108af02584142cc5d18b741b2b6ee4b21dac3778
hambira <hambira.tudu@edgenetworks.in>
2021-07-14 17:10:38 +0530

    add Extra User

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Shital!</p>"
+    return """<p>Hello, Shital & Hambira!</p>
+                Welcome to the CI/CD !
+            """
 
 
 if __name__ == "__main__":
